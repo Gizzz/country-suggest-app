e433143d3c4d33d47c566039a792dc728de4240b
Gizzz <bulatkhusainov@yandex.ru>
2018-09-28 02:01:56 +0300

    add arrow navigation on search results

--- src/CountrySuggest/CountrySuggest.jsx
+++ src/CountrySuggest/CountrySuggest.jsx
@@ -4,10 +4,13 @@ import PropTypes from 'prop-types';
 import './CountrySuggest.css';
 
 class CountrySuggest extends React.Component {
-  state = {
+  initialState = {
     country: '',
     isSuggestionsOpen: false,
+    activeSuggestionIndex: null,
   }
+
+  state = this.initialState
 
   handleInputChange = (e) => {
     const country = e.target.value;
@@ -15,9 +18,46 @@ class CountrySuggest extends React.Component {
     this.props.onCountryChange(country);
   }
 
+  handleInputKeydown = (e) => {
+    const data = this.props.data;
+    if (data.length === 0) {
+      return;
+    }
+
+    // prevent cursor jumping on input when navigating with arrows
+    if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+      e.preventDefault();
+    }
+
+    if (e.key === 'ArrowDown') {
+      if (this.state.activeSuggestionIndex === null) {
+        this.setState({ activeSuggestionIndex: 0 });
+      } else if ((this.state.activeSuggestionIndex + 1) < data.length) {
+        this.setState((state) => ({ activeSuggestionIndex: state.activeSuggestionIndex + 1 }));
+      }
+    } else if (e.key === 'ArrowUp') {
+      if (this.state.activeSuggestionIndex === null) {
+        this.setState({ activeSuggestionIndex: data.length - 1 });
+      } else if ((this.state.activeSuggestionIndex - 1) >= 0) {
+        this.setState((state) => ({ activeSuggestionIndex: state.activeSuggestionIndex - 1 }));
+      }
+    } else if (e.key === 'Enter') {
+      const activeSuggestionIndex = this.state.activeSuggestionIndex;
+      if (activeSuggestionIndex !== null) {
+        this.setState({
+          ...this.initialState,
+          country: data[activeSuggestionIndex].name,
+        });
+      }
+    }
+  }
+
   createSuggestionClickHandler = (country) => {
     const handler = function() {
-      this.setState({ country, isSuggestionsOpen: false });
+      this.setState({
+        ...this.initialState,
+        country,
+      });
     }
 
     return handler.bind(this);
@@ -25,7 +65,7 @@ class CountrySuggest extends React.Component {
 
   render() {
     const data = this.props.data;
-    const suggestionList = data.map((countryData) => {
+    const suggestionList = data.map((countryData, index) => {
       const flagField = this.props.flagField;
       const flagUrl = typeof flagField === 'string' ? countryData[flagField] : flagField(countryData);
       const flagStyles = {
@@ -34,7 +74,11 @@ class CountrySuggest extends React.Component {
       };
 
       return (
-        <li key={countryData.name} onClick={this.createSuggestionClickHandler(countryData.name)}>
+        <li
+          className={index === this.state.activeSuggestionIndex ? 'active' : ''}
+          key={countryData.name}
+          onClick={this.createSuggestionClickHandler(countryData.name)}
+        >
           <span className="flag" style={flagStyles}></span>
           <span className="text">{countryData.name}</span>
         </li>
@@ -43,7 +87,7 @@ class CountrySuggest extends React.Component {
 
     return (
       <div className="country-suggest">
-        <input type="text" value={this.state.country} onChange={this.handleInputChange} />
+        <input type="text" value={this.state.country} onChange={this.handleInputChange} onKeyDown={this.handleInputKeydown} />
         {
           suggestionList.length > 0 && this.state.isSuggestionsOpen &&
           <ul className="suggestions">{suggestionList}</ul>
